f559337d9148b2a336008cecf82c89ce0a6d62d8
Aaron <aacheng17@gmail.com>
2020-12-21 16:19:28 -0500

    Revert "first commit"
    This reverts commit 8d5779c300b771d1768e0940ffcd8d2abfcdeca0.

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const fetch = require("node-fetch");
 var cors = require("cors");
-const fs = require('fs');
 
+const LANGS = [ 'af', 'sq', 'am', 'ar',  'hy',  'az', 'eu',
+                'be',    'bn', 'bs', 'bg', 'ca',  'ceb', 'ny', 'zh-cn',
+                'zh-tw', 'co', 'hr', 'cs', 'da',  'nl',  'en', 'eo',
+                'et',    'tl', 'fi', 'fr', 'fy',  'gl',  'ka', 'de',
+                'el',    'gu', 'ht', 'ha', 'haw', 'iw',  'hi', 'hmn',
+                'hu',    'is', 'ig', 'id', 'ga',  'it',  'ja', 'jw',
+                'kn',    'kk', 'km', 'ko', 'ku',  'ky',  'lo', 'la',
+                'lv',    'lt', 'lb', 'mk', 'mg',  'ms',  'ml', 'mt',
+                'mi',    'mr', 'mn', 'my', 'ne',  'no',  'ps', 'fa',
+                'pl',    'pt', 'pa', 'ro', 'ru',  'sm',  'gd', 'sr',
+                'st',    'sn', 'sd', 'si', 'sk',  'sl',  'so', 'es',
+                'su',    'sw', 'sv', 'tg', 'ta',  'te',  'th', 'tr',
+                'uk',    'ur', 'uz', 'vi', 'cy',  'xh',  'yi', 'zu' ];
 const app = express();
 const port = process.env.PORT || 3000;
 const numTranslations = 10;
@@ -19,9 +31,34 @@ app.use(cors());
 
 // Test route, visit localhost:3000 to confirm it's working
 // should show 'Hello World!' in the browser
-app.get("/", (req, res) => {
-  fs.writeFileSync('data.json', "Hello");
-  res.send("Hello World!");
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// Main route
+app.get("/api/search", async (req, res) => {
+  try {
+    var s = req.query.q;
+    console.log("Receving request string: " + s);
+    var i = 0;
+    for (i=0;i<numTranslations;i++) {
+      var lang = (i == numTranslations-1) ? 'en' : LANGS[Math.floor(Math.random() * LANGS.length)];
+      console.log('https://translation.googleapis.com/language/translate/v2?target='+lang+'&key='+process.env.GOOGLE_API_KEY+'&q='+encodeURI(s));
+      var response = await fetch(
+        'https://translation.googleapis.com/language/translate/v2?target='+lang+'&key='+process.env.GOOGLE_API_KEY+'&q='+encodeURI(s)
+      );
+      var json = await response.text();
+      s = JSON.parse(json).data.translations[0].translatedText;
+    }
+
+    return res.json({
+      success: true,
+      "results": s,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
